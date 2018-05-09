1219f8a7ffee284317d48d4606a2311e73ef5d17
Chr1sSully <christopherpetersullivan@gmail.com>
2018-05-09 16:37:30 +1000

    Rock paper scissors
    added a simple rock paper scissors game
    by ChrisSully
    play with !rps r
    to play rock, p for paper, s for scissors

--- pathfinderbot.py
+++ pathfinderbot.py
@@ -139,6 +139,57 @@ async def _bot():
     """Is the bot cool?"""
     await bot.say('Yes, the bot is cool.')
 
+@bot.command()
+async def rps(*, message: str):
+    # Change user input to full lenght string for to say back later when telling who won
+    if message.lower() == 'r':
+        player_choice = 'rock'
+    elif message.lower() == 'p':
+        player_choice = "paper"
+    elif message.lower() == 's':
+        player_choice = 'scissors'
+    else:
+        await bot.say('Please try again using "!rps" + "r" or "p" or "s" to pick your play')
+
+    # print("player: ", player_choice)
+
+    # Get a random selection for the bots choice
+    selection = [
+    'rock',
+    'paper',
+    'scissors']
+    bot_choice = random.choice(selection)
+    # print("bot: ", bot_choice)
+
+    # Game logic:
+    # 3x if/elif for the players 3 possible inputs
+    # 2x if/elifs inside these to check if there is a win or loss in that game
+    # else catches if there is a draw
+    # TODO - lots of repetition in this section, could win/loss be decided by a function??
+    if player_choice == 'rock':
+        if bot_choice == 'scissors':
+            await bot.say('Your {} beats my {}! YOU WIN'.format(player_choice, bot_choice))     
+        elif bot_choice == 'paper':
+            await bot.say('My {} beats your {}! YOU LOSE'.format(bot_choice, player_choice))
+            
+    elif player_choice == 'paper':
+        if bot_choice == 'rock':
+            await bot.say('Your {} beats my {}! YOU WIN'.format(player_choice, bot_choice))    
+        elif bot_choice == 'scissors':
+            await bot.say('My {} beats your {}! YOU LOSE'.format(bot_choice, player_choice))
+    
+    elif player_choice == 'scissors':
+        if bot_choice == 'paper':
+            await bot.say('Your {} beats my {}! YOU WIN'.format(player_choice, bot_choice))
+            
+        elif bot_choice == 'rock':
+            await bot.say('My {} beats your {} ! YOU LOSE'.format(bot_choice, player_choice))
+            
+    else:
+     # player_choice == bot_choice:
+        await bot.say('Your {} draws with my {}.'.format(player_choice, bot_choice))
+ 
+
 
 # Run Bot
 with open('token.txt') as token_file:
